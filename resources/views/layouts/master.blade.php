a27d14c4dc0f688649f558d3e044e1adafa119bf
JasmeetSinghChhabra <js.chhabra1994@gmail.com>
2019-10-21 19:24:43 -0400

    added facade

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,11 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href=" {{ URL::to('css/custom.css') }} ">
 </head>
 
 <body>
     @include('partials.header')
-    @yield('content')
-    hi</body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
 
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.master')
+
 @section('content')
-<h1> some content</h1>
-<p> {{ 2 == 2 ? "Hello True" : "Does not equal" }}
+<div class="row">
+    <div class="col-md-12">
+        <h1> some content</h1>
+        @if(true)
+        <p> true</p>
+        @else
+        <p>false</p>
+        @endif
+        <hr>
+        @for($i =0; $i < 5; $i++) <p>{{ $i+1}}. Iteration</p>
+            @endfor
+    </div>
+</div>
 @endsection
